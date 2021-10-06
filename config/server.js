62710cd3e958338fa36f937f2b31779848c2b418
Paidninja <paidninja.js@gmail.com>
2021-10-06 16:11:01 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e0280f10c671ee1611fca6f3ce32cbaa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ceb23de1-e5f6-411b-9add-794622d2d874'
+};
